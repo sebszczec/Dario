2f4a60bb94cc2b8d78faab4166621c5515fe68a7
Sebastian Szczecina <sebszczec@wp.pl>
2023-12-04 19:43:26 +0100

    just before refactoring

--- player.gd
+++ player.gd
@@ -6,8 +6,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-enum GameSate {Idle, Run, Jump, Attack}
-var currentState = GameSate.Idle
+enum GameStateByte {Attack, Jump, Idle, Run}
+var state = []
 
 var isJumping = false
 var isAttacking = false
@@ -16,6 +16,10 @@ var isAttacking = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
+	state.append(false) # Attack
+	state.append(false) # Jump
+	state.append(true) # Idle
+	state.append(false) # Run
 	pass
 
 func _process(delta):
@@ -27,18 +31,22 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	else:
 		isJumping = false
+		state[GameStateByte.Jump] = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_jump") and not isJumping:
 		velocity.y = JUMP_VELOCITY
 		isJumping = true
+		state[GameStateByte.Jump] = true
 		
 	# Handle Attack
 	if Input.is_action_pressed("ui_attack"):
 		isAttacking = true
+		state[GameStateByte.Attack] = true
 		
 	if Input.is_action_just_released("ui_attack"):
 		isAttacking = false
+		state[GameStateByte.Attack] = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -47,7 +55,14 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if velocity.x == 0:
+		state[GameStateByte.Idle] = true
+		state[GameStateByte.Run] = false
+	else:
+		state[GameStateByte.Idle] = false
+		state[GameStateByte.Run] = true
+	
 	move_and_slide()
 
 
