a4e8f94e131bc2782350e9cfd92688c4660b6e9e
Sebastian Szczecina <sebszczec@wp.pl>
2023-12-13 11:43:48 +0100

    working on fireball

--- fireball.gd
+++ fireball.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
 
+var direction = Vector2(-2, 1)
+var speed = 200
 
 func _physics_process(delta):
-	pass
+	velocity = direction * speed
+	
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().name == "TileMap":
+			self.position.x = 300
+			self.position.y = 256
 
