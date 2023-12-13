92b8e1cacd59b3ab6098938c46e567689c33a122
Sebastian Szczecina <sebszczec@wp.pl>
2023-12-13 19:06:43 +0100

    working on explosion

--- fireball.gd
+++ fireball.gd
@@ -1,7 +1,17 @@
 extends CharacterBody2D
 
-var direction = Vector2(-2, 1)
-var speed = 200
+@onready var explosion = $Explosion
+@onready var ball = $Ball
+@onready var ballCollision = $BallCollision
+@onready var fire1 = $Fire1
+@onready var fire2 = $Fire2
+
+@export_category("Positioning")
+@export var direction = Vector2(-2, 1)
+@export var speed = 200
+
+func _ready():
+	pass
 
 func _physics_process(delta):
 	velocity = direction * speed
@@ -9,8 +19,14 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if collision.get_collider().name == "TileMap":
-			self.position.x = 300
-			self.position.y = 256
+		ball.visible = false
+		ballCollision.disabled = true
+		fire1.emitting = false
+		fire2.emitting = false
+		speed = 0
+		explosion.explode()
 
+
+
+func _on_explosion_exploded():
+	queue_free()
